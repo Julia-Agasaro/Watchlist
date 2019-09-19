261c4c2f2bbf28e5595158b802c468c54a41c268
Julia Agasaro <agasarojulia@gmail.com>
2019-09-19 12:59:17 +0200

    first commit

---
+++ start.sh
@@ -0,0 +1,6 @@
+ export MOVIE_API_KEY='0a05ea57a37e91924e9ee789ef7f8fa9'
+ export SECRET_KEY=Flask WTF Secret Key
+ export MAIL_USERNAME=agasarojulia@gmail.com
+ export MAIL_PASSWORD=God is able
+
+python3.6 manage.py server
